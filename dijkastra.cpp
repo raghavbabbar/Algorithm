6e7b14f0d4005cf59fb8afb397be4c1480326816
raghavbabbar <raghavbabbar9@gmail.com>
2017-03-22 09:33:38 +0530

    graph

--- dijkastra.cpp
+++ dijkastra.cpp
@@ -12,86 +12,88 @@ using namespace std;
 int min(int[],int[],int);
 int main(int argc, const char * argv[])
 {
-    int wadj[10][10];//w8 adjency matrix
-    int n,edges,srcV,destV,weight;
-    cout<<"\n enter the number of vertices and edges";
-    cin>>n>>edges;
-    for(int i=1;i<=n;i++)
-    for(int j=1;j<=n;j++)
-    wadj[i][j]=INFINITY;
-    cout<<"\n enter the source ,destination vertex and their weight";
-    for (int i=0; i<edges; i++)
-    {
-        cin>>srcV>>destV>>weight;
-        wadj[srcV][destV]=weight;
-        wadj[destV][srcV]=weight;
-    }
-    //DIJKASTRA START
-    int sh[10]={0};
-    int cost[10];
+    int wadj[6][6]={INFINITY,4,1,INFINITY,INFINITY,INFINITY,
+                      4,INFINITY,2,1,6,INFINITY,
+                      1,2,INFINITY,INFINITY,5,INFINITY,
+                      INFINITY,1,INFINITY,INFINITY,3,4,
+                      INFINITY,6,5,3,INFINITY,6,
+                      INFINITY,INFINITY,INFINITY,4,6,INFINITY
+    
+    
+    };//w8 adjency matrix
+    int n=6;
+    int cost[10]={0};
     int path[10]={0};
-    int start;
-    cin>>start;
-    for(int i=1;i<=n;i++)
-    { if(wadj[start][i]==INFINITY)
-    {cost[i]=INFINITY;
-    }
+    int sh[10]={0};
+    int source;
+    cout<<"\n enter the source vertex:";
+    cin>>source;
+    source--;
+    for (int i=0; i<n;i++) {
+        if(wadj[source][i]!=INFINITY)
+        {cost[i]=wadj[source][i];
+            path[i]=source+1;
+        }
         else
-        {
-            cost[i]=wadj[start][i];
-            path[i]=start;
+        {cost[i]=INFINITY;
         }
-    
     }
-    
-    sh[start]=1;
-    cost[start]=0;
-    int flag=0;
-    while(!flag)
-    { int j;
-    j=min(cost,sh,n);
-        sh[j]=0;
-        for(int i=1;i<=n;i++)
+    sh[source]=1;
+    cost[source]=0;
+        int flag=1;
+    int j;
+    while(flag)
+    {j=min(cost,sh,6);
+        sh[j]=1;
+        for(int i=0;i<n;i++)
+        { cout<<"\ni="<<i<<"sh="<<sh[i]<<" w8="<<wadj[j][i];
+            
+            if(sh[i]!=1)
+        {if(wadj[j][i]!=INFINITY)
         {
-         if(sh[i]!=1)
-         {
-         if(wadj[j][i]!=INFINITY)
-         if(wadj[j][i]+cost[j]<cost[i])
-         {cost[i]=wadj[j][i]+cost[j];
-             path[i]=j;
-         }
-         }
+            if((cost[j]+wadj[j][i])<cost[i])
+        {cost[i]=cost[j]+wadj[j][i];
+            path[i]=j+1;
         }
-        flag=1;
-        for(int i=1;i<=n;i++)
+        }
+        
+        }
+            
+        }
+        
+        flag=0;
+        for(int i=0;i<n;i++)
             if(sh[i]==0)
-            {flag=0;
+            {   flag=1;
                 break;
             }
+    
     }
-    // to display the shortest path from source vertex & cost;
+    int dest;
+    cout<<"\n eneter the destination vertex";
+    cin>>dest;
+    int shortpath[10];
 
-    cout<<"\n enter the destination vertex to know its shorets disdance from source vertex";
-    int d;
-    cin>>d;
-    cout<<"cost="<<cost[d];
-    int ss=path[d];
-    int i=0;
-    int shortestPAth[10];
-    while(ss!=start)
-    {shortestPAth[i]=ss;
-        i++;
-        ss=path[ss];
+    cout<<"\n path from "<<source+1<<"  To "<<dest<<"  is ="<<source<<" ->";
+    int m=path[dest-1];
+    int index=0;
+    cout<<"\nii="<<index;
+    while (m!=source+1) {
+           cout<<"\nii="<<index;
+        shortpath[index] = m;
+        m=path[m];
+        index++;
+        
     }
-   cout<<"\n shortest path is "<<path[d];
-    for(int t=0;t<i;t++)
-        cout<<"->"<<shortestPAth[t];
+    
+    for(int k=index-1;k>=0;k--)
+        cout<<" ->"<<shortpath[k];
 return 0;
 }
 int min(int cost[],int sh[],int n)
 { int min_index;
 int min1=9999;
-for(int i=1;i<=n;i++)
+for(int i=0;i<n;i++)
 {
 if(sh[i]==0)
 { if(cost[i]<min1)
@@ -102,7 +104,7 @@ if(sh[i]==0)
 }
 
 }
-
+cout<<"\n returning"<<min_index<<"\n";
 return min_index;
 
 }
